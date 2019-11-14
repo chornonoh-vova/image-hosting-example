13812f57806c52076a46663b31c55ec8e1337c21
Владимир Черноног <chernonog.vova@gmail.com>
2019-11-14 12:13:49 +0200

    Add 2 volumes to store images

--- write-service/src/app.js
+++ write-service/src/app.js
@@ -1,12 +1,39 @@
+import makeCallback from '@hbvhuwe/image-hosting-commons/express-callback.js';
 import express from 'express';
+import fs from 'fs';
 import multer from 'multer';
 
 import imageController from './controllers/index.js';
 
-import makeCallback from '@hbvhuwe/image-hosting-commons/express-callback.js';
+const volumes = fs.readdirSync('/images');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    const destinations = [];
+
+    volumes.forEach(volume => {
+      destinations.push(
+          fs.promises.readdir('/images/' + volume)
+              .then(files => ['/images/' + volume, files.length]));
+    });
+
+    Promise.all(destinations => {
+      let dest = '';
+      let max = 0;
+
+      destinations.forEach(destination => {
+        if (destination[1] > max) {
+          dest = destination[0];
+        }
+      });
+
+      cb(null, dest);
+    });
+  },
+});
 
 const app = express();
-const upload = multer({dest: '/images/'});
+const upload = multer({storage});
 
 // upload image api
 app.post(
